8d4897fb5f7e82a5c4b245129193d8f60aef44e6
soumikplus <csoumik10@gmail.com>
2024-12-29 01:38:57 +0530

    commit from soumik 9

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 import util
 import os
 
 app = Flask(__name__, static_folder='static')
+CORS(app)
 
 @app.route('/')
 def index():
--- static/index.js
+++ static/index.js
@@ -26,9 +26,9 @@ function onClickedEstimatePrice() {
   var location = document.getElementById("uiLocations").value;
   var estPrice = document.getElementById("uiEstimatedPrice");
 
-  // var url = "/predict_home_price";
+  var url = "/predict_home_price";
   // var url = "http://127.0.0.1:5000/predict_home_price";
-  var url = "https://house-price-prediction-5zp7.onrender.com/predict_home_price";
+  // var url = "https://house-price-prediction-5zp7.onrender.com/predict_home_price";
 
 
   $.post(url, {
@@ -47,9 +47,9 @@ function onClickedEstimatePrice() {
 
 function onPageLoad() {
   console.log("document loaded");
-  // var url = "/get_location_names";
+  var url = "/get_location_names";
   // var url = "http://127.0.0.1:5000/get_location_names";
-  var url = "https://house-price-prediction-5zp7.onrender.com/get_location_names";
+  // var url = "https://house-price-prediction-5zp7.onrender.com/get_location_names";
 
   $.get(url, function(data, status) {
     console.log("got response for get_location_names request");

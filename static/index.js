1b45fc142b459ba319f57d78d0c6d1de9153e48d
soumikplus <csoumik10@gmail.com>
2024-12-29 00:22:59 +0530

    commit from soumik 6

--- static/index.js
+++ static/index.js
@@ -26,8 +26,8 @@ function onClickedEstimatePrice() {
   var location = document.getElementById("uiLocations").value;
   var estPrice = document.getElementById("uiEstimatedPrice");
 
-  var url = "/predict_home_price";
-  // var url = "http://127.0.0.1:5000/predict_home_price"; 
+  // var url = "/predict_home_price";
+  var url = "http://127.0.0.1:5000/predict_home_price"; 
 
   $.post(url, {
     total_sqft: parseFloat(sqft.value),
@@ -45,8 +45,8 @@ function onClickedEstimatePrice() {
 
 function onPageLoad() {
   console.log("document loaded");
-  var url = "/get_location_names";
-  // var url = "http://127.0.0.1:5000/get_location_names";
+  // var url = "/get_location_names";
+  var url = "http://127.0.0.1:5000/get_location_names";
   $.get(url, function(data, status) {
     console.log("got response for get_location_names request");
     if (data) {
